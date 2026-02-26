dd2aa2c6a7006015c1bfc37f9f0de89840c38959
mohsin <mohsinkhan08115@gmail.com>
2026-02-27 04:04:51 +0500

    Remove serviceAccountKey from tracking

--- api/index.py
+++ api/index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.routes import auth, patients  # include other routes if needed
+from app.routes import auth, patients, medical_records, queue
 
 app = FastAPI(
     title=settings.APP_NAME,
@@ -21,9 +21,8 @@ app.add_middleware(
 
 app.include_router(auth.router)
 app.include_router(patients.router)
-# include other routers if needed:
-# app.include_router(medical_records.router)
-# app.include_router(queue.router)
+app.include_router(medical_records.router)
+app.include_router(queue.router)
 
 @app.get("/test")
 def test():
