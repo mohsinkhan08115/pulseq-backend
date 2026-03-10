a07623a808ed1cefec38652aa486365855980b59
mohsin <mohsinkhan08115@gmail.com>
2026-03-11 04:52:26 +0500

    Fix: CORS methods

--- api/index.py
+++ api/index.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=False,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
 )
 
